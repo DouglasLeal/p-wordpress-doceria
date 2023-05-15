3a5272fa4d29de406df7063e30933bc16ee6dd38
Douglas Leal <douglas.leal@live.com>
2023-05-15 20:43:11 -0300

    page produtos

---
+++ page-produtos.php
@@ -0,0 +1,87 @@
+<?php /* Template Name: Produtos */ ?>
+<?php get_header(); ?>
+
+<main class="produtos">
+    <div class="container">
+        <h1>Produtos</h1>
+
+        <div class="produto-itens">
+            <div class="produto-item">
+                <img src="./assets/img/bolo.jpg" alt="Bolo">
+                <div class="produto-item-texto">
+                    <h2>Bolos</h2>
+                    <p>Venha conhecer nossos bolos feitos de diversos saberos e coberturas. É uma delícia que vai
+                        deixar você com água na boca. O bolo é feito com ingredientes de qualidade e tem um sabor
+                        irresistível. Você pode encomendar o seu pelo nosso site ou pelo telefone. Aproveite essa
+                        oferta especial e garanta o seu pedaço de felicidade.</p>
+                </div>
+            </div>
+
+            <div class="produto-item produto-item-reverse">
+                <img src="./assets/img/brownie.jpg" alt="Brownie">
+                <div class="produto-item-texto">
+                    <h2>Brownies</h2>
+                    <p>Venha experimentar nossos deliciosos brownies! Temos vários sabores e coberturas para você
+                        escolher. Nossos brownies são feitos com ingredientes de qualidade e muito amor. Aproveite
+                        nossa promoção especial: na compra de dois brownies, leve o terceiro de graça! Não perca
+                        essa oportunidade de saborear uma sobremesa incrível na nossa doceria!</p>
+                </div>
+            </div>
+
+            <div class="produto-item produto-item">
+                <img src="./assets/img/docinhos.png" alt="Docinhos">
+                <div class="produto-item-texto">
+                    <h2>Docinhos</h2>
+                    <p>Se você está procurando por docinhos deliciosos e de qualidade, venha conhecer a nossa
+                        doceria! Temos uma variedade de sabores e tamanhos para todos os gostos e ocasiões. Você
+                        pode escolher entre brigadeiros, beijinhos, cajuzinhos, casadinhos e muito mais. Todos os
+                        nossos docinhos são feitos com ingredientes frescos e selecionados, e embalados com carinho.
+                        Faça já o seu pedido e surpreenda-se com o nosso sabor!</p>
+                </div>
+            </div>
+
+            <div class="produto-item produto-item-reverse">
+                <img src="./assets/img/mousse.jpg" alt="Mousse">
+                <div class="produto-item-texto">
+                    <h2>Mousses</h2>
+                    <p>Você está procurando uma sobremesa deliciosa e refrescante? Então venha conhecer as nossas
+                        mousses! Temos diversos sabores para agradar todos os paladares: chocolate, maracujá, limão,
+                        morango e muito mais. As nossas mousses são feitas com ingredientes de qualidade e têm uma
+                        textura leve e cremosa. Você pode levar para casa ou saborear na nossa doceria, que tem um
+                        ambiente aconchegante e um atendimento especial. Não perca tempo e venha experimentar as
+                        nossas mousses. Você vai se surpreender!
+                    </p>
+                </div>
+            </div>
+
+            <div class="produto-item produto-item">
+                <img src="./assets/img/pudim.jpg" alt="Pudim">
+                <div class="produto-item-texto">
+                    <h2>Pudins</h2>
+                    <p>Você está procurando uma sobremesa deliciosa? Então venha conhecer os nossos
+                        pudins! Temos vários sabores para você escolher, como leite condensado, chocolate, coco,
+                        maracujá e muito mais. Todos os nossos pudins são feitos com ingredientes de qualidade e sem
+                        conservantes. Eles são perfeitos para qualquer ocasião, seja um almoço em família, um lanche
+                        da tarde ou uma festa de aniversário. Não perca tempo e faça já o seu pedido! Estamos
+                        esperando por você!
+                    </p>
+                </div>
+            </div>
+
+            <div class="produto-item produto-item-reverse">
+                <img src="./assets/img/torta.jpg" alt="Torta">
+                <div class="produto-item-texto">
+                    <h2>Tortas</h2>
+                    <p>Você está procurando uma sobremesa deliciosa e caseira? Então venha conhecer as nossas tortas. Temos diversos sabores para todos os gostos: chocolate, morango,
+                        limão, banana, maçã e muito mais. Todas as nossas tortas são feitas com ingredientes frescos
+                        e de qualidade, e você pode escolher entre a massa tradicional ou integral. Aproveite a
+                        nossa promoção: na compra de uma torta inteira, você ganha 50 docinhos. Venha
+                        nos visitar e experimente as nossas tortas. Você vai se surpreender com o sabor e a textura.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
