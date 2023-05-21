5c2792dfdfab1f8e31be66c9a77f4c4e8c24d36b
Douglas Leal <douglas.leal@live.com>
2023-05-21 19:28:50 -0300

    Page sobre modificada

--- page-sobre.php
+++ page-sobre.php
@@ -3,24 +3,11 @@
 
 <main class="sobre">
     <div class="container">
-        <h1>Sobre Nós</h1>
-        <p>
-            A nossa história começou há dez anos, quando eu e minha irmã decidimos abrir uma pequena loja de doces
-            caseiros. Nós sempre adoramos cozinhar e experimentar novos sabores, e queríamos compartilhar essa
-            paixão com as pessoas. Foi assim que nasceu a <span>DoceMia</span>, um lugar onde cada doce é feito com amor e
-            carinho.
-        </p>
+        <h1><?php the_title(); ?></h1>
 
-        <p>
-            Desde então, nós crescemos muito e ampliamos o nosso cardápio, mas nunca perdemos a essência do nosso
-            trabalho. Nós continuamos buscando a excelência em cada detalhe, desde a escolha dos ingredientes até a
-            apresentação dos doces. Nós queremos que você se sinta em casa e se delicie com as nossas criações.
-        </p>
-
-        <p>
-            Venha nos visitar e descubra o que faz da <span>DoceMia</span> um lugar especial. Nós temos certeza que você
-            vai se apaixonar pelos nossos doces!
-        </p>
+        <div>
+            <?php the_content(); ?>
+        </div>
     </div>
 </main>
 
